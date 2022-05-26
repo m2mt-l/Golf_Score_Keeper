a88623fa2fa3ae84e773f76768f4452bdaf4eabe
m2mt-l <ta28ma2@gmail.com>
2022-05-26 23:28:37 +0000

    fix: lint

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,28 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Score {
-    position: number,
-    par?: number,
-    stroke?: number,
-
+    position: number;
+    par?: number;
+    stroke?: number;
 }
 @Component({
-  selector: 'app-grid',
-  templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+    selector: 'app-grid',
+    templateUrl: './grid.component.html',
+    styleUrls: ['./grid.component.css'],
 })
 export class GridComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    displayedColumns: string[] = ['holeNumber', 'parForHall', 'name'];
 
-  displayedColumns: string[] = ['holeNumber', 'parForHall', 'name'];
-
-  scores: Score[] = [
-      {position: 1, par: undefined, stroke: undefined},
-      {position: 2, par: undefined, stroke: undefined},
-  ]
-
+    scores: Score[] = [
+        { position: 1, par: undefined, stroke: undefined },
+        { position: 2, par: undefined, stroke: undefined },
+    ];
 }
