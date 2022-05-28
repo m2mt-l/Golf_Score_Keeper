3ea70777efb88256f53bf838a27995cec8f2a043
m2mt-l <ta28ma2@gmail.com>
2022-05-28 01:18:17 +0000

    feat: add input and total numbers binding

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -14,11 +14,47 @@ export class GridComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
-
+    displayedTotalColumns: string[] = ['underOverPar', 'totalPar', 'totalStroke']
     displayedColumns: string[] = ['holeNumber', 'parForHall', 'name'];
+    totalParForPlayer: number = 0;
 
     scores: Score[] = [
         { position: 1, par: undefined, stroke: undefined },
         { position: 2, par: undefined, stroke: undefined },
     ];
+
+    onKeyParForHall(position: number, value: string): void {
+        this.scores[position - 1].par = Number(value);
+        console.table(this.scores);
+    }
+
+    onKeyParForPlayer(position: number, value: string): void {
+        this.scores[position - 1].stroke = Number(value);
+        console.table(this.scores);
+    }
+
+    sumParForHall(): number {
+        let totalParForHall: number = 0;
+        for (let score of this.scores) {
+            if(score.par != undefined) {
+                totalParForHall += score.par
+            } 
+        }
+        return totalParForHall;
+    }
+
+    sumParForPlayer(): number {
+        let totalParForPlayer: number = 0;
+        for (let score of this.scores) {
+            if(score.stroke != undefined) {
+                totalParForPlayer += score.stroke
+            } 
+        }
+        return totalParForPlayer;
+    }
+
+    parResult(): number {
+        return this.sumParForHall() - this.sumParForPlayer();
+    }
+
 }
